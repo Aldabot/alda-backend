86ecc776b45485ee7f386b5bea7900dba7c50e79
Dirk Hornung <dirkhornung91@gmail.com>
2019-02-13 18:50:03 +0100

    removed graphql-yoga playground

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,3 @@
 'use strict';
 export { default as fbWebhook } from './fbWebhook'
 export { server as graphqlServer } from 'alda-graphql-server'
-export { playground as graphqlPlayground } from 'alda-graphql-server'
